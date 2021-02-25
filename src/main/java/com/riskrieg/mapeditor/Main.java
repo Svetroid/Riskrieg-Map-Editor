db44d7bf29eb84d8238b8162325ba73ac173daff
Svetroid <Svetroid@users.noreply.github.com>
2021-02-25 18:01:16 -0500

    Adds dark/light theme switching based on current OS theme.

--- src/main/java/com/riskrieg/mapeditor/Main.java
+++ src/main/java/com/riskrieg/mapeditor/Main.java
@@ -1,12 +1,40 @@
 package com.riskrieg.mapeditor;
 
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLaf;
 import com.formdev.flatlaf.FlatLightLaf;
+import com.jthemedetecor.OsThemeDetector;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
 
+  public static final OsThemeDetector themeDetector = OsThemeDetector.getDetector();
+
   public static void main(String[] args) {
-    FlatLightLaf.install();
+    if (themeDetector.isDark()) {
+      FlatDarkLaf.install();
+    } else {
+      FlatLightLaf.install();
+    }
+    themeDetector.registerListener(isDark -> {
+      if (isDark) {
+        try {
+          UIManager.setLookAndFeel(new FlatDarkLaf());
+          FlatLaf.updateUI();
+        } catch (UnsupportedLookAndFeelException e) {
+          e.printStackTrace();
+        }
+      } else {
+        try {
+          UIManager.setLookAndFeel(new FlatLightLaf());
+          FlatLaf.updateUI();
+        } catch (UnsupportedLookAndFeelException e) {
+          e.printStackTrace();
+        }
+      }
+    });
 
     SwingUtilities.invokeLater(() -> {
       try {
@@ -17,5 +45,6 @@ public class Main {
       }
     });
   }
+
 
 }
